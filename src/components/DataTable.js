5b7b628654f7605a5893b9b7057533ef8f80b4e4
Daniel Yuschick <danyuschick@gmail.com>
2020-11-28 11:17:19 +0200

    basic table structure in place

---
+++ src/components/DataTable.js
@@ -0,0 +1,101 @@
+import React, { Fragment, useState, useEffect, useCallback } from "react";
+import styled from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  CircularProgress,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+export default function BasicTable({ fetch, caption }) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const result = await fetch();
+      console.log("DATA", result.data);
+      setData([...data, ...result.data]);
+    } catch {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }, [data, fetch]);
+
+  useEffect(() => {
+    if (data.length) return;
+    fetchData();
+  }, [data, fetchData]);
+  const classes = useStyles();
+
+  return (
+    <Fragment>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label={caption}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Date</TableCell>
+              <TableCell>User ID</TableCell>
+              <TableCell>Old Value</TableCell>
+              <TableCell>New Value</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell component="th" scope="row">
+                  {item.timestamp}
+                </TableCell>
+                <TableCell>{item.id}</TableCell>
+                <TableCell>{item.diff[0].oldValue}</TableCell>
+                <TableCell>{item.diff[0].newValue}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <FooterContainer>
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <Fragment>
+            {error && (
+              <Typography color="error" role="alert">
+                We had problems fetching your data. Please try again.
+              </Typography>
+            )}
+            <Button variant="contained" color="primary" onClick={fetchData}>
+              {error ? "Retry" : "Load more"}
+            </Button>
+          </Fragment>
+        )}
+      </FooterContainer>
+    </Fragment>
+  );
+}
+
+const FooterContainer = styled.div`
+  display: grid;
+  grid-gap: var(--spacing-two);
+  margin: var(--spacing-two) 0;
+  place-items: center;
+`;
---
+++ src/routes/App/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+import api from "../../lib/api";
+import DataTable from "../../components/DataTable";
+
+const App = () => {
+  return (
+    <StyledPage>
+      <DataTable
+        caption="A table of historical user updates"
+        fetch={api.getUsersDiff}
+      />
+      <DataTable
+        caption="A table of historical project updates"
+        fetch={api.getProjectsDiff}
+      />
+    </StyledPage>
+  );
+};
+
+const StyledPage = styled.main`
+  display: grid;
+  grid-gap: var(--spacing-three);
+  padding: var(--spacing-two) var(--spacing-three);
+`;
+
+export default App;
