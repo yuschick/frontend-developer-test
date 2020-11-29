020e1c15ad8363ef93cf07b241481b7c33dc76af
Daniel Yuschick <danyuschick@gmail.com>
2020-11-29 12:53:14 +0200

    Updates

---
+++ precache-manifest.1124fdd3b01301f7182f7a7c364f7da2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a777f6f658599656b4b44d85df430937",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15922e28045825253951",
+    "url": "/static/js/2.a1c0049f.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.a1c0049f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e460b39a4f1811ba48",
+    "url": "/static/js/main.4191a834.chunk.js"
+  },
+  {
+    "revision": "f34d5340acad89fdcd14",
+    "url": "/static/js/runtime-main.bd01b83d.js"
+  }
+]);
--- src/types/sortDirection.js
+++
@@ -1,5 +0,0 @@
-export const SortDirection = Object.freeze({ asc: "asc", desc: "desc" });
-export const InvertSortDirection = Object.freeze({
-  [SortDirection.asc]: SortDirection.desc,
-  [SortDirection.desc]: SortDirection.asc,
-});
